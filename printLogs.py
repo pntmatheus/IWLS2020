cd78429bcfd61623b83db0ebb68c6a60c055a557
pntmatheus <pnt.matheus@gmail.com>
2020-06-16 12:46:57 -0300

    Results analisys

--- printLogs.py
+++ printLogs.py
@@ -1,7 +1,11 @@
 import os
 
 if __name__ == "__main__":
+
     for root, dirs, files in os.walk("Contest_AIGs_INFO", topdown=False):
         for name in files:
             with open(os.path.join(root, name), "r") as arquivo:
-                print(arquivo.read())
+                linhas = arquivo.readlines()
+                for linha in linhas:
+                    if "OK!!" not in linha:
+                        print("%s" % (linha.replace("\n", "")))
