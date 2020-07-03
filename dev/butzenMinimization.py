21a3087336d32cd38bded873c2cbdc7a44de251d
pntmatheus <pnt.matheus@gmail.com>
2020-07-02 22:23:46 -0300

    Train multiprocess

--- dev/butzenMinimization.py
+++ dev/butzenMinimization.py
@@ -1,18 +1,79 @@
 import time
 import bisect
 
-from Functions.pla import pla_obj_factory
+
+def pega_termos_literais(literais, termos):
+    for t in termos:
+        print(len(t.get_input()))
+
+
+def retorna_contagem(lista_termos, qt_inputs, literais=None):
+    print("QT-INPUTS ===> %s " % qt_inputs)
+    if not literais:
+        retorno = list()
+
+        tabela_quantitativo = list()
+        # inicializar a tabela de quantitativos
+        # TODO deixar pronto para qualquer número de saídas
+
+        inicial = time.time()
+
+        # Pq 2 x inputs??
+        # Pq considero A e A!
+        for i in range(2 * qt_inputs):
+            # [<qt_0>, <qt_1>]
+            tabela_quantitativo.append([0, 0])
+
+        for termo in lista_termos:
+            if termo.output == "0":
+                for i, char in enumerate(termo.get_input()):
+                    posicao = 2 * i + int(char)
+                    tabela_quantitativo[posicao][0] += 1
+            else:
+                for i, char in enumerate(termo.get_input()):
+                    posicao = 2 * i + int(char)
+                    tabela_quantitativo[posicao][1] += 1
+
+        dict_quantitativo = {}
+
+        inicial = time.time()
+        for i, variavel in enumerate(tabela_quantitativo):
+            # Aqui coloco no dicionario [<qt_1>, <diff_qt_1_E_qt_0>]
+            dict_quantitativo[i] = [variavel[1], (variavel[1] - variavel[0])]
+
+        maior_var = max(dict_quantitativo.items(), key=lambda x: x[1])
+        bisect.insort(retorno, maior_var[0])
+
+        dict_quantitativo.pop(maior_var[0])
+        maior_var = max(dict_quantitativo.items(), key=lambda x: x[1])
+        bisect.insort(retorno, maior_var[0])
+
+        return retorno
+    else:
+        print("PAPAI")
+        print(literais)
 
 
 def pega_mais_leva_pra_um(qt_inputs, termos, ja_contadas=False):
 
+    primeiros = retorna_contagem(termos, qt_inputs)
+    print(primeiros)
+
+    pega_termos_literais(primeiros, termos)
+
+    time.sleep(15)
     retorno = list()
 
     tabela_quantitativo = list()
     # inicializar a tabela de quantitativos
     # TODO deixar pronto para qualquer número de saídas
+
     inicial = time.time()
+
+    # Pq 2 x inputs??
+    # Pq considero A e A!
     for i in range(2 * qt_inputs):
+        # [<qt_0>, <qt_1>]
         tabela_quantitativo.append([0, 0])
     print("Tempo FOR criacao tabela_quantitativo ===> %f" % (time.time() - inicial))
 
@@ -28,11 +89,14 @@ def pega_mais_leva_pra_um(qt_inputs, termos, ja_contadas=False):
                 tabela_quantitativo[posicao][1] += 1
     print("Tempo FOR que conta as variaveis ===> %f" % (time.time() - inicial))
 
+    for tab in tabela_quantitativo:
+        print(tab)
 
     dict_quantitativo = {}
 
     inicial = time.time()
     for i, variavel in enumerate(tabela_quantitativo):
+        # Aqui coloco no dicionario [<qt_1>, <diff_qt_1_E_qt_0>]
         dict_quantitativo[i] = [variavel[1], (variavel[1] - variavel[0])]
 
     maior_var = max(dict_quantitativo.items(), key=lambda x: x[1])
@@ -47,14 +111,3 @@ def pega_mais_leva_pra_um(qt_inputs, termos, ja_contadas=False):
     print("Tempo CONTA os maiores ===> %f" % (time.time() - inicial))
 
     return retorno
-
-
-if __name__ == '__main__':
-    tempo_inicial = time.time()
-
-    pla_obj = pla_obj_factory("/media/sf_PastaUbuntuServer/ex90.total.pla")
-    # pla_obj = pla_obj_factory("ex41.total.pla")
-
-    print(pega_mais_leva_pra_um(pla_obj.get_qt_inputs(), pla_obj.get_termos()))
-
-    print("Tempo final %f " % (time.time() - tempo_inicial))
--- multiProcessResults.py
+++ multiProcessResults.py
@@ -10,6 +10,24 @@ def best_aig(number):
     process = check_output(["python3.8",
                             "IWLS2020.py",
                             "-i",
+                            "iwls_competition_0323/ex%02d.train.pla" % number,
+                            "-o",
+                            "Contest_AIGs/ex%02d.aig" % number,
+                            "--mltest",
+                            "iwls_competition_0323/ex%02d.valid.pla" % number,
+                            "--dont-clean-tmp",
+                            "--supress",
+                            "--verbose"], stderr=subprocess.PIPE)
+
+    with open("Contest_AIGs_INFO/ex%02d.train.txt" % number, "w") as file:
+        file.write(process.decode())
+
+
+def best_aig_old(number):
+    print("%02d" % number)
+    process = check_output(["python3.8",
+                            "IWLS2020.py",
+                            "-i",
                             "PLA_files/ex%02d.total.pla" % number,
                             "-o",
                             "Contest_AIGs/ex%02d.aig" % number,
@@ -28,7 +46,5 @@ if __name__ == "__main__":
     set_start_method("spawn")
     tempo_inicial = time()
     with Pool(processes=50) as pool:
-        #pool.map(best_aig, range(10))
         pool.map(best_aig, range(100))
-        #print(pool.map(best_aig, [0,1]))
     print("Runtime: " + str(time()-tempo_inicial))
